fe665725de53a5b9c414cd3bd50b9296c9ca8ba6
lijiying <leogogo.ljy@gmail.com>
2021-07-03 18:15:58 +0800

    feat: 添加deploy命令

---
+++ scripts/deploy.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# 任意命令出错时终止运行
+set -e
+
+echo "打包dist..."
+npm run build
+echo "打包完成..."
+
+echo "需要进行远程部署吗? (Y/n)"
+read answer
+if [ "$answer" != "" ] && [ "$answer" != "Y" ] && [ "$answer" != "y" ] ;then
+  echo "本地打包完成!"
+  exit 0
+fi
+
+git checkout -b gh-pages
+git add -f dist
+git commit -m 'build project'
+git subtree push --prefix dist origin gh-pages
