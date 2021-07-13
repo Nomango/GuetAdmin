2ba2fa006d9dfe435d4ba561b02e1840116505f0
lijiying <leogogo.ljy@gmail.com>
2021-07-13 23:02:05 +0800

    fix: 又又携带cookie

--- src/components/Upload/EditorImage.vue
+++ src/components/Upload/EditorImage.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="upload-container">
     <el-button
-      :style="{background:color,borderColor:color}"
+      :style="{ background: color, borderColor: color }"
       icon="el-icon-upload"
       size="mini"
       type="primary"
-      @click=" dialogVisible=true"
+      @click="dialogVisible = true"
     >
       上传图片
     </el-button>
@@ -19,6 +19,7 @@
         :before-upload="beforeUpload"
         class="editor-slide-upload"
         :action="action"
+        :headers="headers"
         list-type="picture-card"
       >
         <el-button size="small" type="primary">
@@ -36,23 +37,27 @@
 </template>
 
 <script>
-
 export default {
-  name: 'EditorSlideUpload',
+  name: "EditorSlideUpload",
   props: {
     color: {
       type: String,
-      default: '#1890ff'
+      default: "#1890ff"
     },
 
     action: {
       type: String,
-      default: ''
+      default: ""
     },
 
     prefix: {
       type: String,
-      default: ''
+      default: ""
+    },
+
+    headers: {
+      type: Object,
+      default: () => {}
     }
   },
   data() {
@@ -60,71 +65,79 @@ export default {
       dialogVisible: false,
       listObj: {},
       fileList: []
-    }
+    };
   },
   methods: {
     checkAllSuccess() {
-      return Object.keys(this.listObj).every(item => this.listObj[item].hasSuccess)
+      return Object.keys(this.listObj).every(
+        item => this.listObj[item].hasSuccess
+      );
     },
 
     handleSubmit() {
-      const arr = Object.keys(this.listObj).map(v => this.listObj[v])
+      const arr = Object.keys(this.listObj).map(v => this.listObj[v]);
       if (!this.checkAllSuccess()) {
         this.$message({
-          message: 'Please wait for all images to be uploaded successfully. If there is a network problem, please refresh the page and upload again!',
+          message:
+            "Please wait for all images to be uploaded successfully. If there is a network problem, please refresh the page and upload again!",
           type: "warning"
-        })
-        return
+        });
+        return;
       }
 
-      this.$emit('successCBK', arr)
-      this.listObj = {}
-      this.fileList = []
-      this.dialogVisible = false
+      this.$emit("successCBK", arr);
+      this.listObj = {};
+      this.fileList = [];
+      this.dialogVisible = false;
     },
 
     handleSuccess(response, file) {
-      const uid = file.uid
-      const objKeyArr = Object.keys(this.listObj)
-      const fileKey = objKeyArr.find(key => key === uid + '')
+      const uid = file.uid;
+      const objKeyArr = Object.keys(this.listObj);
+      const fileKey = objKeyArr.find(key => key === uid + "");
 
       if (fileKey && response) {
-        const { data } = response
-        const fileItem = this.listObj[fileKey]
-        fileItem.url = this.prefix + data.suffix // response.files.file
-        fileItem.hasSuccess = true
-        return
+        const { data } = response;
+        const fileItem = this.listObj[fileKey];
+        fileItem.url = this.prefix + data.suffix; // response.files.file
+        fileItem.hasSuccess = true;
+        return;
       }
     },
 
     handleRemove(file) {
-      const uid = file.uid
-      const objKeyArr = Object.keys(this.listObj)
+      const uid = file.uid;
+      const objKeyArr = Object.keys(this.listObj);
 
       for (let i = 0, len = objKeyArr.length; i < len; i++) {
         if (this.listObj[objKeyArr[i]].uid === uid) {
-          delete this.listObj[objKeyArr[i]]
-          return
+          delete this.listObj[objKeyArr[i]];
+          return;
         }
       }
     },
 
     beforeUpload(file) {
-      const _self = this
-      const _URL = window.URL || window.webkitURL
-      const fileName = file.uid
-      this.listObj[fileName] = {}
+      const _self = this;
+      const _URL = window.URL || window.webkitURL;
+      const fileName = file.uid;
+      this.listObj[fileName] = {};
       return new Promise((resolve, reject) => {
-        const img = new Image()
-        img.src = _URL.createObjectURL(file)
+        const img = new Image();
+        img.src = _URL.createObjectURL(file);
         img.onload = function() {
-          _self.listObj[fileName] = { hasSuccess: false, uid: file.uid, width: this.width, height: this.height }
-        }
-        resolve(true)
-      })
+          _self.listObj[fileName] = {
+            hasSuccess: false,
+            uid: file.uid,
+            width: this.width,
+            height: this.height
+          };
+        };
+        resolve(true);
+      });
     }
   }
-}
+};
 </script>
 
 <style lang="scss" scoped>
